43c8eed90593e39b23ab628cfc4022a3c8121fb0
Daniel Mack <daniel@zonque.org>
2019-12-04 20:54:42 +0100

    ASoC: simple-card: add support for codec-to-codec links
    DAI links in ASoC can be turned into codec-to-codec links by populating
    the .params field. This patch adds support for this to the simple-card
    driver and exposes the feature via three new device-tree properties that
    configure the pcm format, the sampling rate and the channel count.
    
    Signed-off-by: Daniel Mack <daniel@zonque.org>

--- include/sound/simple_card_utils.h
+++ include/sound/simple_card_utils.h
@@ -48,6 +48,7 @@ struct asoc_simple_priv {
 		struct snd_soc_dai_link_component platforms;
 		struct asoc_simple_data adata;
 		struct snd_soc_codec_conf *codec_conf;
+		struct snd_soc_pcm_stream c2c_params;
 		unsigned int mclk_fs;
 	} *dai_props;
 	struct asoc_simple_jack hp_jack;
@@ -121,7 +122,10 @@ void asoc_simple_convert_fixup(struct asoc_simple_data *data,
 void asoc_simple_parse_convert(struct device *dev,
 			       struct device_node *np, char *prefix,
 			       struct asoc_simple_data *data);
-
+void asoc_simple_parse_c2c_params(struct device *dev,
+				  struct device_node *np,
+				  char *prefix,
+				  struct snd_soc_pcm_stream *dest);
 int asoc_simple_parse_routing(struct snd_soc_card *card,
 				      char *prefix);
 int asoc_simple_parse_widgets(struct snd_soc_card *card,
