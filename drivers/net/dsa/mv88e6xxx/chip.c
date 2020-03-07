311f457c29651c077a4233e7a001d727a43f639f
Daniel Mack <daniel@zonque.org>
2020-03-07 07:19:24 +0100

    net: dsa: mv88e6xxx: don't force settings on CPU port
    On hardware with a speed-reduced link to the CPU port, forcing the MAC
    settings won't allow any packets to pass. The PHY will negotiate the
    maximum possible speed, so let's allow the MAC to work with whatever
    is available.
    
    Signed-off-by: Daniel Mack <daniel@zonque.org>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -3292,7 +3292,7 @@ static int mv88e6xxx_setup_port(struct mv88e6xxx_chip *chip, int port)
 	 * state to any particular values on physical ports, but force the CPU
 	 * port and all DSA ports to their maximum bandwidth and full duplex.
 	 */
-	if (dsa_is_cpu_port(ds, port) || dsa_is_dsa_port(ds, port))
+	if (dsa_is_dsa_port(ds, port))
 		err = mv88e6xxx_port_setup_mac(chip, port, LINK_FORCED_UP,
 					       SPEED_MAX, DUPLEX_FULL,
 					       PAUSE_OFF,
